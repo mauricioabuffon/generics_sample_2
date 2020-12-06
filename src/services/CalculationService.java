87937526b8a755a010348631134f91f7848362f5
Mauricio Antonio Buffon <mauricioabuffon@gmail.com>
2020-12-06 15:58:34 -0300

    Project created

---
+++ src/services/CalculationService.java
@@ -0,0 +1,22 @@
+package services;
+
+import java.util.List;
+
+import entities.Product;
+
+public class CalculationService {
+
+	public static <T extends Comparable<T>> T max(List<T> list) {
+		if (list.isEmpty()) {
+			throw new IllegalStateException("List can not be empty");
+		}
+		T max = list.get(0);
+		for (T item : list) {
+			if (item.compareTo(max) > 0){
+				max = item;
+			}
+		}
+		return max;
+	}
+	
+}
